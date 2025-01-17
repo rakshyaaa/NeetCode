201e9cc356f266a71abe44094326484efa03210d
Rakshya Pandey <c00580017@louisiana.edu>
2025-01-17 10:48:22 -0600

    resume later

---
+++ encode_decode.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        print('encode')
+
+    def decode(self, s: str) -> List[str]:
+        print('decode')
+
+
+if __name__ == "__main__":
+
+    solution = Solution()
+    solution.encode('apple')
+    solution.decode('ball')
