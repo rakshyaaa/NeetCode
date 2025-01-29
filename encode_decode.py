a04093d9dddf841610c5af411ca89cebf2e1cfec
Rakshya Pandey <c00580017@louisiana.edu>
2025-01-29 13:01:36 -0600

    encode-decode

--- encode_decode.py
+++ encode_decode.py
@@ -1,16 +1,30 @@
 from typing import List
 
 class Solution:
-
     def encode(self, strs: List[str]) -> str:
-        print('encode')
+        res = ""  
+        for i in strs:
+            res +=  str(len(i)) + "#" + i
+        return res 
 
     def decode(self, s: str) -> List[str]:
-        print('decode')
-
-
+        print(s)
+        res, i  =  [], 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            print(i,j)
+            length = int(s[i:j])
+            print(length)
+            i = j + 1
+            res.append(s[i:i+length])
+            i += length
+        return res
+            
 if __name__ == "__main__":
-
     solution = Solution()
-    solution.encode('apple')
-    solution.decode('ball')
+    encoded = solution.encode( ["rakshya","pandey"])
+    print(encoded)
+    decoded = solution.decode(encoded)
+    print(decoded)
