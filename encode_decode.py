ba0e6f7dbdcf44b0b7e073c79c92abfda9764643
Rakshya Pandey <c00580017@louisiana.edu>
2025-01-29 13:10:25 -0600

    changes in encode decode

--- encode_decode.py
+++ encode_decode.py
@@ -4,23 +4,22 @@ class Solution:
     def encode(self, strs: List[str]) -> str:
         res = ""  
         for i in strs:
-            res +=  str(len(i)) + "#" + i
+            res += str(len(i)) + '#' + i
         return res 
 
     def decode(self, s: str) -> List[str]:
-        print(s)
-        res, i  =  [], 0
+        res, i = [],0
+
         while i < len(s):
             j = i
             while s[j] != '#':
                 j += 1
-            print(i,j)
             length = int(s[i:j])
-            print(length)
-            i = j + 1
-            res.append(s[i:i+length])
-            i += length
-        return res
+            res.append(s[j+1 : j+1+length])
+
+            i = j + 1 + length
+
+        return res        
             
 if __name__ == "__main__":
     solution = Solution()
